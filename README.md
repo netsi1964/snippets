c868d2698c3a53ee22952b9253d5ff2f481b1bb0
Sten Hougaard <netsi1964@gmail.com>
2015-04-22 23:52:45 +0200

    imageSideBySideInSVG.js first version

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # snippets
-Various snippets to run in Browser Developer toolbar
+The Developer toolbar of the various browsers is king!
+You can do so many great things there, by using simple javascript and DOM manipulation.
+Here are various snippets to run in Browser Developer toolbar.
+
+## Author
+Sten Hougaard, [@netsi1964](http://twitter.com/netsi1964).
+
+## imagesSideBySideInSVG.js
+This is a cute litle javascript which will generate a SVG containing grouped by image height, the images on current website.
+You can see [Example output on codepen](http://codepen.io/netsi1964/pen/cc1973442ee943ba2e8d8547de07ba16). 
--- imagesSideBySideInSVG.js
+++ imagesSideBySideInSVG.js
@@ -9,7 +9,14 @@ var images = [],
 	iMaxWidth = -1,
 	iMinHeight = 100000,
 	iMaxHeight = -1,
-	heightLookup = {};
+	heightLookup = {},
+	maxWidth = 5000,
+	leftMargin = 200,
+	imageMargin = 5,
+	sText = "<text x=\"XXpx\" y=\"YYpx\" style=\"font-family:Arial;font-size:12px;fill:#000;\">HEIGHT px</text>",
+	iTextOffsetY = 20,
+	sLine = "<path d=\"m X1,Y1 L X2, Y2\" style=\"stroke:grey; stroke-width: 1px;stroke-dasharray:5,5;\" />",
+	sLog = "<?xml version=\"1.0\" standalone=\"no\" ?><!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\"><svg width=\"" + maxWidth + "\" height=\"5000\" viewBox=\"0 0 " + maxWidth + " 5000\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xml:space=\"preserve\" style=\"fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:1.41421;\">";
 oImages = document.querySelectorAll("img");
 [].forEach.call(oImages, function (ele, i) {
 	images.push(ele.src);
@@ -38,11 +45,16 @@ function eachWidth(ele, height) {
 }
 var yy = 0;
 for (foundHeight in heightLookup) {
-	var xx = 0;
+	var xx = leftMargin;
 	widths = heightLookup[foundHeight];
+	sLog += "<g>";
+	sLog += sText.replace("XX", imageMargin).replace("YY", yy + imageMargin + iTextOffsetY).replace("HEIGHT", foundHeight);
+	sLog += sLine.replace("X1", 0).replace("Y1", yy).replace("X2", maxWidth).replace("Y2", yy);
+	sLine
 	widths.map(function (ele) {
-		xx += parseInt(eachWidth(ele, foundHeight, xx), 10)
+		xx += parseInt(eachWidth(ele, foundHeight, xx), 10) + imageMargin;
 	});
-	yy += parseInt(foundHeight, 10);
+	sLog += "</g>";
+	yy += parseInt(foundHeight, 10) + imageMargin;
 }
-console.log(sLog);
+console.log(sLog + "</svg>");
